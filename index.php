0272af936383cc77afce89bece52dfc5abb12b52
Flo0611 <tiflo2000@hotmail.fr>
2019-11-28 11:49:52 +0100

    test

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ session_start();
 <html lang="fr">
 
 <head>
-    <title>Centre équestre - Oui</title>
+    <title>Centre équestre - Accueil</title>
     <!-- Meta tag Keywords -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8" />
