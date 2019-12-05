c41156dce43a3663ec899926bc25fce1b2db16f9
DYLAN JOLIVET <50867887+dylaanjolivet@users.noreply.github.com>
2019-12-05 09:32:10 +0100

    allclass

--- all.class.php
+++ all.class.php
@@ -8,10 +8,6 @@
   require_once'class/photo_cheval.class.php';
   require_once'class/chevaux_fav.class.php';
   require_once'class/race_chevaux.class.php';
-<<<<<<< HEAD
-  
-=======
   require_once'class/actu.class.php';
->>>>>>> 224fe9e5d9c66dc3903c6ac66708dd1d6f24432e
 
 ?>
