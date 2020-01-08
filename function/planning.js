1eff383275f032e5ed9d3e9e5a0c39c154e7f838
Flo0611 <tiflo2000@hotmail.fr>
2020-01-08 11:26:02 +0100

    securisation
    securisation

--- function/planning.js
+++ function/planning.js
@@ -5,7 +5,6 @@ function changement_couleur(nom_jour)
   var bouton_ecoute = bouton[i]
   bouton_ecoute.addEventListener('click', function () {
     document.querySelector(nom_jour).style.setProperty("background-color", "orange")
-    console.log("ok")
   })
 }
 
