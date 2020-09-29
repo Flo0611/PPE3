c60f23d8d5e7d2a78c8ef10783cd24793f391421
Flo0611 <tiflo2000@hotmail.fr>
2020-09-29 12:10:32 +0200

    ajout test avec fichier

---
+++ test/all.class.php
@@ -0,0 +1,30 @@
+<?php
+
+  require_once'class/membre.class.php';
+  require_once'class/stage.class.php';
+  require_once'class/ville.class.php';
+  require_once'class/contact.class.php';
+  require_once'class/chevaux.class.php';
+  require_once'class/photo_cheval.class.php';
+  require_once'class/chevaux_fav.class.php';
+  require_once'class/race_chevaux.class.php';
+  require_once'class/actu.class.php';
+  require_once'class/balades.class.php';
+  require_once'class/newsletter.class.php';
+  require_once'class/galop.class.php';
+  require_once'class/pension.class.php';
+  require_once'class/note.class.php';
+  require_once'class/photo_profil.class.php';
+  require_once'class/images_membre.class.php';
+  require_once'class/images_actu.class.php';
+  require_once'class/presentation.class.php';
+  require_once'class/box.class.php';
+  require_once'class/designation.class.php';
+  require_once'class/cours.class.php';
+  require_once'class/horaires.class.php';
+  require_once'class/jours.class.php';
+  require_once'class/inscription_cours.class.php';
+  require_once'class/inscription_activite.class.php';
+  require_once'class/maladie.class.php';
+
+?>
